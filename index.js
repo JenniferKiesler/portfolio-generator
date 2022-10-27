746db0287e49f4f9cac075e84cf16919672c16d0
JenniferKiesler <111549041+JenniferKiesler@users.noreply.github.com>
2022-10-26 21:23:32 -0500

    generates html string

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+import inquirer from 'inquirer'
+import generateHTML from './generateHTML.js'
+import fs from 'fs'
+
 // Your application should prompt the user for information like their name, location, bio, LinkedIn URL, and GitHub URL. Feel free to add any additional prompts you think of.
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: "What's your location?"
+    },
+    {
+        type: 'input',
+        name: 'bio',
+        message: 'Tell me about yourself.'
+    },
+    {
+        type: 'input',
+        name: 'linkedinURL',
+        message: "What's your LinkedIn URL?"
+    },
+    {
+        type: 'input',
+        name: 'githubURL',
+        message: "What's your Github URL?"
+    }
+])
+    .then((answers) => {
+        console.log(answers)
+        const html = generateHTML(answers)
+        console.log(html)
+
+        
+    })
+    .catch(err => console.log(err))
